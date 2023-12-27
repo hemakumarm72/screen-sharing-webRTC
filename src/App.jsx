9088e6339f9775537c9aba4da01b74c3bc15702a
Hemakumar <hemakumarm72@gmail.com>
2023-12-27 23:58:09 +0530

    vidoes call

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css';
+import { Box, Heading, Container } from '@chakra-ui/react';
+import Notifications from './Components/Notifications';
+import Options from './Components/Options';
+import VideoPlayer from './Components/VidoePlayer';
 
 function App() {
   return (
-    <div className='App'>
-      <p>Hello world</p>
-    </div>
+    <Box>
+      <Container maxW='1200px' mt='8'>
+        <Heading as='h2' size='2xl'>
+          {' '}
+          Video Chat App{' '}
+        </Heading>
+        <VideoPlayer />
+        <Options />
+        <Notifications />
+      </Container>
+    </Box>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
+import { ContextProvider } from './Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ContextProvider>
   </React.StrictMode>
 );
 
