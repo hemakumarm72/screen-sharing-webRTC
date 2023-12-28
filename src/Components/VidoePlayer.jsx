f2f4907491207ad906a84b5c612b8a6932d86ede
Hemakumar <hemakumarm72@gmail.com>
2023-12-28 13:05:42 +0530

    update mic and vidoe mute

--- src/Components/VidoePlayer.jsx
+++ src/Components/VidoePlayer.jsx
@@ -1,11 +1,29 @@
 import { Grid, Box, Heading } from '@chakra-ui/react';
 import { SocketContext } from '../Context';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 const VideoPlayer = () => {
+  const [vidoe, setVidoe] = useState(true);
+  const [mic, setMic] = useState(true);
+
   const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } =
     useContext(SocketContext);
+  function muteMic() {
+    stream.getAudioTracks().forEach((track) => {
+      return (track.enabled = !track.enabled);
+    });
+    setMic(mic ? false : true);
+  }
 
+  function muteCam() {
+    stream.getVideoTracks().forEach((track) => {
+      return (track.enabled = !track.enabled);
+    });
+    setVidoe(vidoe ? false : true);
+  }
+  useEffect(() => {
+    console.log(vidoe, mic);
+  }, [vidoe, mic]);
   return (
     <Grid justifyContent='center' templateColumns='repeat(2, 1fr)' mt='12'>
       {/* my video */}
@@ -15,13 +33,26 @@ const VideoPlayer = () => {
             <Heading as='h5'>{name || 'Name'}</Heading>
             <video
               playsInline
-              muted
               ref={myVideo}
               autoPlay
               width='600'
               style={{ transform: 'scaleX(-1)' }}
             />
           </Grid>
+          <button
+            className='button'
+            style={{ backgroundColor: !vidoe ? 'red' : '#ea4c89' }}
+            onClick={muteCam}
+          >
+            Vidoes Mute
+          </button>
+          <button
+            className='button'
+            style={{ backgroundColor: !mic ? 'red' : '#ea4c89' }}
+            onClick={muteMic}
+          >
+            Mic Mute
+          </button>
         </Box>
       )}
       {/* user's video */}
